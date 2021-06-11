81821578ee52c249167068540aebd0340a1002a0
sueryong <32319690+sueryong@users.noreply.github.com>
2021-06-11 17:36:42 +0900

    asdf

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Panel {
-    constructor(name, htmlPath) {
-        this.panelName = name;
+    constructor(panelName, htmlPath) {
+        this.panelName = panelName;
         this.path = htmlPath;
         this.lastY = 0;
         this.onPanel = true;
@@ -9,23 +9,29 @@ class Panel {
         this.setting();
     }
 
-    setting () {
+    setting = () => {
         this.makePanel();
         this.setEvent();
         this.setTouchEvent();
-        this.appendHTML();
     }
 
-    setEvent () {
-        this.topElement.childNodes[1].addEventListener('click',(e)=>{this.hidePanel(e, this)});
-        this.topElement.childNodes[3].addEventListener('transitionend', (e)=>{this.transitionEnd(e, this)});
-        this.topElement.childNodes[3].addEventListener('transitionstart', (e)=>{this.transitionStart(e, this)});
+    setEvent = () => {
+        this.topElement.childNodes[1].addEventListener('click',(e)=>{this.hidePanel()});
+        this.topElement.childNodes[3].addEventListener('transitionend', (e)=>{this.transitionEnd(e)});
+        this.topElement.childNodes[3].addEventListener('transitionstart', (e)=>{this.transitionStart(e)});
+    };
+
+    setTouchEvent = () => {
+        this.topElement.childNodes[3].childNodes[1].addEventListener("touchstart", (e) =>{this.handleStart(e);});
+        this.topElement.childNodes[3].childNodes[1].addEventListener("touchend", (e) =>{this.handleEnd(e);});
+        this.topElement.childNodes[3].childNodes[1].addEventListener("touchmove", (e) =>{this.handleMove(e);});
     }
 
-    makePanel () {
+
+    makePanel = () => {
         document.querySelector('body').innerHTML +=`
-                       <div data-status="0" class=${this.panelName}>
-                          <div class="${this.panelName}-mask" style="position: absolute; display: none; width: 100vw; height: 100vh; background-color: grey; opacity: 0.4; top: 0"></div>
+                       <div data-status="0" class=${this.panelName} style="position: absolute; bottom: 0">
+                          <div class="${this.panelName}-mask" style="position: fixed; top: 0; display: none; width: 100vw; height: 100vh; background-color: grey; opacity: 0.4;"></div>
                           <div class="${this.panelName}-panel" style="position: fixed; width: 100vw; height: 61.646875vh; background-color: white; transform: translate(0%, 0%); transition: all ease-in-out 0.3s 0s;">
                               <div class="${this.panelName}-drag-bar" style="width: 100vw; height: 34px; background-color: grey"></div>
                               <div class="${this.panelName}-html"></div>
@@ -35,116 +41,118 @@ class Panel {
         this.panelElement = $(`.${this.panelName}-panel`);
     }
 
-    hidePanel (e, panel) {
-        if(e === undefined){
-            this.topElement.childNodes[3].style.transform = 'translate(0%, 0%)';
-            this.topElement.childNodes[1].style.display = 'none';
-            this.topElement.dataset.status = '0';
-        }else {
-            e.preventDefault();
-            panel.topElement.childNodes[3].style.transform = 'translate(0%, 0%)';
-            panel.topElement.childNodes[1].style.display = 'none';
-            panel.topElement.dataset.status = '0';
-        }
+    hidePanel = () => {
+        // document.querySelector(`.${this.panelName}`).childNodes[1].style.display = 'none';
+        // document.querySelector(`.${this.panelName}`).childNodes[3].style.transform = 'translate(0%, 0%)';
+        // document.querySelector(`.${this.panelName}`).dataset.status = '0';
+
+        this.topElement.childNodes[1].style.display = 'none';
+        this.topElement.childNodes[3].style.transform = 'translate(0%, 0%)';
+        this.topElement.dataset.status = '0';
     }
 
-    showPanel () {
-        this.topElement.childNodes[3].style.transform = 'translate(0%, -100%)';
+    showPanel = () => {
+        // document.querySelector(`.${this.panelName}`).childNodes[1].style.display = 'block';
+        // document.querySelector(`.${this.panelName}`).childNodes[3].style.transform = 'translate(0%, -100%)';
+        // document.querySelector(`.${this.panelName}`).dataset.status = '1';
         this.topElement.childNodes[1].style.display = 'block';
+        this.topElement.childNodes[3].style.transform = 'translate(0%, -100%)';
         this.topElement.dataset.status = '1';
-    }
+    };
 
-    setTouchEvent () {
-        this.topElement.childNodes[3].childNodes[1].addEventListener("touchstart", (e) =>{this.handleStart(e, this);}, false);
-        this.topElement.childNodes[3].childNodes[1].addEventListener("touchend", (e) =>{this.handleEnd(e, this);}, false);
-        this.topElement.childNodes[3].childNodes[1].addEventListener("touchmove", (e) =>{this.handleMove(e, this);}, false);
-    }
-
-    handleStart (e, panel) {
+    handleStart = (e) => {
         e.preventDefault();
         document.querySelector('body').style.overflow = 'hidden';
-        panel.topElement.childNodes[3].style.transition = '';
-        panel.lastY = e.touches[0].screenY;
+        this.topElement.childNodes[3].style.transition = '';
+        this.lastY = e.touches[0].screenY;
     }
 
-    handleMove (e, panel) {
+    handleMove = (e) => {
         e.preventDefault();
 
-        let panelTranslateY = Number(panel.panelElement.css('transform').split(',')[5].slice(0, -1));
-        let deltaY = Number((panel.lastY - e.touches[0].screenY).toFixed(3));
+        let panelTranslateY = Number(this.panelElement.css('transform').split(',')[5].slice(0, -1));
+        let deltaY = Number((this.lastY - e.touches[0].screenY).toFixed(3));
 
-        if(panel.onPanel === false){
-            if(panelTranslateY >= 0 || panelTranslateY <= panel.panelElement.height()){
-                panel.onPanel = true;
+        if(this.onPanel === false){
+            if(panelTranslateY >= 0 || panelTranslateY <= this.panelElement.height()){
+                this.onPanel = true;
             }
             return;
         }
 
-        if(panel.panelElement.height() < - panelTranslateY + deltaY && panel.onPanel === true){
-            panel.topElement.childNodes[3].style.transform = 'translate(0%, -100%)';
-            panel.onPanel = false;
+        if(this.panelElement.height() < - panelTranslateY + deltaY && this.onPanel === true){
+            this.topElement.childNodes[3].style.transform = 'translate(0%, -100%)';
+            this.onPanel = false;
             return;
         }
 
-        if(0 > - panelTranslateY + deltaY && panel.onPanel === true){
-            panel.topElement.childNodes[3].style.transform = 'translate(0%, 0%)';
-            panel.onPanel = false;
+        if(0 > - panelTranslateY + deltaY && this.onPanel === true){
+            this.topElement.childNodes[3].style.transform = 'translate(0%, 0%)';
+            this.onPanel = false;
             return;
         }
 
         e.target.parentNode.style.transform = `translate(0%, ${panelTranslateY - deltaY}px)`;
-        panel.lastY = e.touches[0].screenY;
+        this.lastY = e.touches[0].screenY;
     }
 
-    handleEnd (e, panel) {
+    handleEnd = (e) => {
         e.preventDefault();
-        panel.onPanel = true;
+        this.onPanel = true;
         document.querySelector('body').style.overflow = 'auto';
         e.target.parentNode.style.transition = 'all ease-in-out 0.3s 0s';
 
-        let panelTranslateY = Number(panel.panelElement.css('transform').split(',')[5].slice(0, -1));
+        let panelTranslateY = Number(this.panelElement.css('transform').split(',')[5].slice(0, -1));
 
-        if(- panelTranslateY > panel.panelElement.height() * 7 / 10){
-            panel.showPanel();
+        if(- panelTranslateY > this.panelElement.height() * 7 / 10){
+            this.showPanel();
         }else {
-            panel.hidePanel(e, panel);
+            this.hidePanel();
         }
     }
 
-    appendHTML () {
+    appendHTML = () => {
         $(`.${this.panelName}-html`).load(this.path);
     }
 
-    appendDragBar (element) {
+    appendDragBarHTML = (element) => {
         $(`.${this.panelName}-drag-bar`).empty();
         $(`.${this.panelName}-drag-bar`).append(element);
     }
 
-    transitionEnd (e, panel) {
+    transitionEnd(e) {
         switch (e.target.parentNode.dataset.status) {
-            case '0': panel.afterHide();
+            case '0': this.afterHide();
                 break;
-            case '1': panel.afterShow();
+            case '1': this.afterShow();
                 break;
             default:
                 break;
         }
     }
 
-    transitionStart (e, panel) {
+    transitionStart(e) {
         switch (e.target.parentNode.dataset.status) {
-            case '0': panel.beforeHide();
+            case '0': this.beforeHide();
                 break;
-            case '1': panel.beforeShow();
+            case '1': this.beforeShow();
                 break;
             default:
                 break;
         }
     }
-    beforeShow () {}
-    beforeHide () {}
-    afterShow () {}
-    afterHide () {}
+    beforeShow = () => {}
+    beforeHide = () => {}
+    afterShow = () => {}
+    afterHide = () => {}
 }
 
 let panel = new Panel('purchase', './product-purchase.html');
+panel.appendHTML();
+
+let panel1 = new Panel('purchase1', './product.html');
+panel1.appendHTML();
+
+
+document.querySelector('.button-1').addEventListener('click', ()=>{console.log(panel.topElement);panel.showPanel()}, false);
+document.querySelector('.button-2').addEventListener('click', ()=>{panel1.showPanel()}, false);
